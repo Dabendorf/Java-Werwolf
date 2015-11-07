dbcf4d2094c0987de17af71b042cd91be3f72a38
Lukas Schramm <lukasschramm@Antjes-MacBook-Pro.local>
2015-11-07 23:45:03 +0100

    ReceiveTextMethode integriert
    Die Graphik besitzt nun die Moeglichkeit Text entgegenzunehmen, welcher
    zur List hinzugefügt wird.

--- src/werwolf/WerwolfGUI.java
+++ src/werwolf/WerwolfGUI.java
@@ -1,6 +1,7 @@
 package werwolf;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
@@ -19,10 +20,8 @@ public class WerwolfGUI {
 	private DefaultListModel<String> chatListModel = new DefaultListModel<String>();
     private JList<String> chatList = new JList<String>(chatListModel);
 	private JTextField textInput = new JTextField();
-	private int anzPlayers;
 	
-	public WerwolfGUI(int anzPlayers) {
-		this.anzPlayers = anzPlayers;
+	public WerwolfGUI() {
 		
 		frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame1.setPreferredSize(new Dimension(600,400));
@@ -33,25 +32,31 @@ public class WerwolfGUI {
 		Container cp = frame1.getContentPane();
 		//cp.setLayout(new GridLayout(8,8));
 		cp.setLayout(new GridBagLayout());
-		//cp.add(chatList, new GridBagFelder(0,0,1,1,1,0.9));
-		//cp.add(textInput, new GridBagFelder(0,1,1,1,1,0.1));
+		cp.add(chatList, new GridBagFelder(0,0,1,1,1,0.9));
+		cp.add(textInput, new GridBagFelder(0,1,1,1,1,0.1));
 		textInput.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				chatListModel.addElement(textInput.getText());
-				textInput.setText("");
+				receiveText(textInput.getText());
 			}
 		});
-		
-		/*for(int a=0;a<8;a++) {
-			for(int b=0;b<8;b++) {
-				cp.add(new PlayerPanel());
-			}
-		}*/
-		//cp.setLayout(new GridBagLayout());
 		
 		frame1.pack();
 		frame1.setLocationRelativeTo(null);
 		frame1.setVisible(true);
+	}
+	
+	public void receiveText(String text) {
+		chatList.setSelectionBackground(new Color(0x33B200));
+		chatListModel.addElement(text);
+		textInput.setText("");
+	}
+	
+	/*public String sendText() {
+		
+	}*/
+	
+	public static void main(String[] args) {
+		new WerwolfGUI();
 	}
 }
