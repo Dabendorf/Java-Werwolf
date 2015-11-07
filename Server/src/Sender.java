500f59cbfce50b0135234173e649dc92583e406f
keraJLi <kerajli@yahoo.de>
2015-11-07 23:39:36 +0100

    src ordner

---
+++ Server/src/Sender.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Sender {
+	private List<?> clients;
+	
+	public Sender() {
+		this.setClients(new ArrayList<>());
+		
+	}
+
+	public List<?> getClients() {
+		return clients;
+	}
+
+	public void setClients(List<?> clients) {
+		this.clients = clients;
+	}
+	
+	public void 
+}
