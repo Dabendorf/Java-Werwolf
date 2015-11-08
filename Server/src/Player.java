01039529ef956acf1fbadabfad1c7f57f3af0807
Toastbrot333 <333toastbrot333@gmail.com>
2015-11-08 02:16:20 +0100

    Implementet basic methods for PLayer class

--- Server/src/Player.java
+++ Server/src/Player.java
@@ -1,5 +1,25 @@
 
 public class Player {
+	private String name;
 	private boolean isAlive;
 	public int countVotes = 0;
+	
+	public Player(String name){
+		this.name = name;
+		isAlive = true;
+	}
+	
+	public void kill(){
+		isAlive = false;
+	}
+	
+	public void reanimate(){
+		isAlive = true;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	
 }
